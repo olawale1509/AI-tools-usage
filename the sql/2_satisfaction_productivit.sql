5d8082b6b2e4499633938ae45b3f3df5fe67d5cd
olawale1509 <balogun.o.yusugh@gmail.com>
2026-06-21 01:34:31 -0600

    ,

--- the sql/2_satisfaction_productivit.sql
+++ the sql/2_satisfaction_productivit.sql
@@ -1,6 +1,7 @@
 -- Which specific AI tools and primary use cases yield the highest percentage of productivity gains, and how does user experience level mediate this relationship?
 -- the data by tool and use case, calculating the average productivity gain and the average hours saved, giving you a straightforward leaderboard.          
 --Objective: Quantify the operational impact of different AI use cases (e.g., Automation, Data Analysis, Translation) to determine the most value-generating tools and whether prior technical expertise is required to unlock this value.
+CREATE VIEW satisfaction_productivity as
 SELECT ai_tool,
     primary_use_case,
     COUNT(user_id) AS total_users,
